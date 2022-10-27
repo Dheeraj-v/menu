64e75f4b87e7765980166d4edbaae8d7912df493
VB <VB>
2022-10-26 18:48:28 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
 import { Component } from '@angular/core';
-
+import { MenuItem } from './models/menu-item';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'menu';
+  title = 'Welcome to Chirpus Pizza';
+  menuItem:MenuItem[]=[
+    {
+      item: 'chicken fingers',
+      category: 'dinner',
+      price: 11.99
+    },
+    {
+      item: 'pizza',
+      category: 'dinner',
+      price: 11.99
+    },
+    {
+      item: 'wings',
+      category: 'sides',
+      price: 8.99
+    },
+    {
+      item: 'breadsticks',
+      category: 'sides',
+      price: 4.99
+    },
+    {
+      item: 'caesar salad',
+      category: 'salads',
+      price: 11.99
+    },
+    {
+      item: 'cinnamon roll',
+      category: 'desert',
+      price: 8.99
+    }
+  ]
 }
